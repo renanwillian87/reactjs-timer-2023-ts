552fe0e653ee8461f9f715b5dc690788df6f7bd3
Renan Santos <renanwillian87@gmail.com>
2023-07-07 14:11:46 -0300

    chore: apply styled component theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from "./components/Button";
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
       <Button variant="primary"/>
       <Button variant="secondary"/>
       <Button variant="success"/>
       <Button variant="danger"/>
-    </>
+    </ThemeProvider>
     
   )
 }
