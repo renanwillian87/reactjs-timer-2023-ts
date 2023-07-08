9c10320213b290b6cbf7b8f878621ec6b1068bc5
Renan Santos <renanwillian87@gmail.com>
2023-07-08 20:30:17 -0300

    chore: styled history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,52 @@
-import { HistoryContainer } from "./styles";
+import { HistoryContainer, HistoryList } from './styles'
 
 export function History() {
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <th>Tarefas</th>
+            <th>Duração</th>
+            <th>Inicío</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
     </HistoryContainer>
   )
 }
